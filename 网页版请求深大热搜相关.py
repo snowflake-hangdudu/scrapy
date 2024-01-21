1c207c69db7059c79f0abd353f48769d3f2b6eea
hangdudu <15551359775@163.com>
2024-01-21 18:45:47 +0800

    1111

---
+++ 网页版请求深大热搜相关.py
@@ -0,0 +1,59 @@
+import requests
+import re
+from lxml import etree
+
+#提取文字的方法
+def extract_text_from_html(html_string):
+    tree = etree.HTML(html_string)
+    text = tree.xpath('string()')
+    return text
+
+
+def extract_text_from_html(html_string):
+    tree = etree.HTML(html_string)
+    text = tree.xpath('string()')
+    return text
+
+#hot_search列表
+hot_search_list = ['深圳大学','深大']
+
+#baseurl
+base_url = 'https://m.weibo.cn/api/container/getIndex?containerid=100103type%3D1%26q%3D%E6%B7%B1%E5%9C%B3%E5%A4%A7%E5%AD%A6&page_type=searchall'
+#第几页
+# url = 'https://m.weibo.cn/api/container/getIndex?containerid=100103type%3D1%26q%3D%E6%B7%B1%E5%9C%B3%E5%A4%A7%E5%AD%A6&page_type=searchall&page=4
+
+# response = requests.get(base_url)
+# json_data = response.json()
+# print(json_data)
+# print(json_data['data']['cards'],'我是列表')
+
+# for i in json_data['data']['cards']:
+#     if(i['card_type'] == 11):
+#         continue
+#     else:
+#         # i['mblog']['text']
+#         # print(i)
+#         print(extract_text_from_html(i['mblog']['text']))
+
+#前五页的内容
+for i in range(1,6):
+    if(i==1):
+        url = base_url
+        print(url)
+        response = requests.get(url)
+        json_data = response.json()
+        for i in json_data['data']['cards']:
+            if(i['card_type'] == 11):
+                continue
+            else:
+                print(extract_text_from_html(i['mblog']['text']))
+                print('---------------------------------------------------------------\n')
+    else:
+        url = base_url + f'&page={i}'
+        print(url)
+        response = requests.get(url)
+        json_data = response.json()
+        for i in json_data['data']['cards']:
+            print(extract_text_from_html(i['card_group'][0]['mblog']['text']))
+            print('---------------------------------------------------------------\n')
+            
---
+++ 网页版请求深大用户.py
@@ -0,0 +1,93 @@
+import requests
+import re
+from lxml import etree
+
+#提取文字的方法
+def extract_text_from_html(html_string):
+    tree = etree.HTML(html_string)
+    text = tree.xpath('string()')
+    return text
+
+#转换链接的方法 
+def mid2url(mid):
+   output = re.sub(r'\\', '', mid)
+   return output
+
+# 发送GET请求
+# response = requests.get('https://m.weibo.cn/api/container/getIndex?t=0&luicode=10000011&lfid=231583&type=uid&value=6239620007&containerid=1076036239620007')
+#第一页连接
+# https://m.weibo.cn/api/container/getIndex?t=0&luicode=10000011&lfid=100103type%3D1%26q%3D%E6%B7%B1%E5%9C%B3%E5%A4%A7%E5%AD%A6&type=uid&value=6239620007&containerid=1076036239620007
+#第二页连接
+# https://m.weibo.cn/api/container/getIndex?t=0&luicode=10000011&lfid=100103type%3D1%26q%3D%E6%B7%B1%E5%9C%B3%E5%A4%A7%E5%AD%A6&type=uid&value=6239620007&containerid=1076036239620007&since_id=4991877749410959
+
+# data = response.text
+# 解析JSON格式的响应内容
+# json_data = response.json()
+
+# 打印深大的属性，总微博数，since_id,自动加载数
+# since_id = json_data['data']['cardlistInfo']['since_id']
+# total = json_data['data']['cardlistInfo']['total']
+# autoLoadMoreIndex = json_data['data']['cardlistInfo']['autoLoadMoreIndex']
+# print(json_data['data']['cardlistInfo']['since_id'])
+# print(json_data['data']['cardlistInfo']['total'])
+# print(json_data['data']['cardlistInfo']['autoLoadMoreIndex'])
+
+# 打印微博内容
+# for i in json_data['data']['cards']:
+    #标题
+    # print()
+    #链接
+    # print(mid2url(i['scheme']))
+    #内容
+    # print( extract_text_from_html(i['mblog']['text']))
+    #
+    
+
+            # item['type'] = 'weibo'
+            # item['title'] = i.xpath('./@mid')[0]
+            # item['url'] = mid2url(i.xpath('./@mid')[0])
+            # item['msg'] = i.xpath('.//p/text()')[0]
+            # item['icon_desc'] = ''
+            # item['hot_value'] = ''
+    
+#获取账户信息
+userInfo_url = 'https://m.weibo.cn/api/container/getIndex?t=0&luicode=10000011&lfid=100103type%3D1%26q%3D%E6%B7%B1%E5%9C%B3%E5%A4%A7%E5%AD%A6&type=uid&value=6239620007&containerid=1005056239620007'
+response = requests.get(userInfo_url)
+json_data = response.json()
+#基础统计量
+    #粉丝数量
+print(json_data['followers_count'])
+    #发稿数量
+
+    #阅读数量
+
+    #点赞数量
+
+
+
+
+
+#拿取前5页的内容
+for i in range(1, 6):
+    if(i == 1):
+        url = 'https://m.weibo.cn/api/container/getIndex?t=0&luicode=10000011&lfid=100103type%3D1%26q%3D%E6%B7%B1%E5%9C%B3%E5%A4%A7%E5%AD%A6&type=uid&value=6239620007&containerid=1076036239620007'
+    else:
+       url = f'https://m.weibo.cn/api/container/getIndex?t=0&luicode=10000011&lfid=100103type%3D1%26q%3D%E6%B7%B1%E5%9C%B3%E5%A4%A7%E5%AD%A6&type=uid&value=6239620007&containerid=1076036239620007&since_id={since_id}'
+    response = requests.get(url)
+    json_data = response.json()
+    since_id = json_data['data']['cardlistInfo']['since_id']
+    
+    print(i)
+    print(since_id)
+    for card in json_data['data']['cards']:
+        # 链接
+        print(mid2url(card['scheme']))
+        # 内容
+        print( extract_text_from_html(card['mblog']['text']))
+            
+
+
+# with open('网页版请求深大用户.txt', 'w', encoding='utf-8') as f:
+#     f.write(data)
+
+
